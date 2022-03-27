8c478c8eb29167d075c09f546319c53600622536
proplayery <102372621+proplayery@users.noreply.github.com>
2022-03-27 18:16:53 +0200

    Add files via upload

---
+++ MailSpammerBot.py
@@ -0,0 +1,53 @@
+from email.mime.text import MIMEText
+import smtplib
+
+
+
+
+
+def menu():
+    print("[1] Option 1")
+    print("[2] Option 2")
+    print("[0] Exit the script.")
+
+option = int(input("Enter your option: "))
+
+while option != 0:
+    if option == 1:
+        user = input("Enter your email address: ")
+        password = input('Enter the password of your email address: ')
+        receiver = input("Enter the email address of your victim: ")
+        smtb = input("Specify the SMTP server: ")
+        port = input("Enter the port: ")
+        text = input("Enter the text of your email: ")
+        amount = int(input("How many emails should be sent: "))
+
+
+
+
+
+        for element in range(amount):
+            sender = user
+            receiver = receiver
+
+            msg = MIMEText(text)
+
+            msg['From'] = user
+            msg['To'] = receiver
+
+
+            with smtplib.SMTP(smtb, port) as server:
+                server.starttls()
+
+                server.login(user, password)
+                server.sendmail(sender, receiver, msg.as_string())
+            print("mail successfully sent")
+
+    elif option == 2:
+        print("Not available")
+    else:
+        print("quit")
+    print()
+    menu()
+    option = int(input("Enter your option: "))
+print("Thanks for using the script")
